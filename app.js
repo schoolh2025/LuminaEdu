30bda9f1aac1fa4158f8758f64e9cc06fa396fad
schoolh2025 <epoint622@gmail.com>
2026-06-27 14:56:35 +0530

    Update Firebase configuration in app.js
    Updated Firebase configuration with new API keys and project details.

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, se
 
 // 🔥 FIREBASE STABLE MODULE PARAMS INITIALIZATION
 const firebaseConfig = {
-    apiKey: "YOUR_REAL_FIREBASE_API_KEY_HERE",
-    authDomain: "luminaedu-ai786.firebaseapp.com",
-    projectId: "luminaedu-ai786",
-    storageBucket: "luminaedu-ai786.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDEXmjIN8w2s2uXk0FTzC7ri4HhLetzV4E",
+  authDomain: "luminaedu-ai786.firebaseapp.com",
+  projectId: "luminaedu-ai786",
+  storageBucket: "luminaedu-ai786.firebasestorage.app",
+  messagingSenderId: "35041307389",
+  appId: "1:35041307389:web:846f981017df7ad1382c94"
 };
 
 const app = initializeApp(firebaseConfig);
